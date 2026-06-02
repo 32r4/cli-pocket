2e574b0d0569f478e6409ba8bf8222becc8caf26
ezra <ezra.ora.c@gmail.com>
2026-06-02 15:35:18 +0800

    Use PowerShell as default Windows shell

--- crates/server/pty/src/platform/windows.rs
+++ crates/server/pty/src/platform/windows.rs
@@ -1,6 +1,11 @@
+const WINDOWS_POWERSHELL_PATH: &str = r"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+const CMD_PATH: &str = r"C:\Windows\System32\cmd.exe";
+
 pub(crate) fn default_shell() -> Vec<String> {
-    if std::path::Path::new(r"C:\Windows\System32\cmd.exe").exists() {
-        vec![r"C:\Windows\System32\cmd.exe".to_string()]
+    if std::path::Path::new(WINDOWS_POWERSHELL_PATH).exists() {
+        vec![WINDOWS_POWERSHELL_PATH.to_string()]
+    } else if std::path::Path::new(CMD_PATH).exists() {
+        vec![CMD_PATH.to_string()]
     } else {
         vec!["powershell.exe".to_string()]
     }
